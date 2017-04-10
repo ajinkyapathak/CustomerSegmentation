f141c13437e9989657b0d025ab8ababc77f35fba
Ajinkya Pathak <ajinkya.pathak@agrostar.in>
2017-04-10 19:00:31 +0530

    jupyter notebooks of classifier added.

--- scripts/naive_bayes.py
+++ scripts/naive_bayes.py
@@ -9,12 +9,9 @@ data = pd.read_csv('~/Projects/FinalYearProject/customer_segmentation.csv', low_
 data = data.as_matrix()
 print data.shape
 
-feature1 = data[:, 7]
-feature2 = data[:, 9]
-feature3 = data[:, 10]
-feature4 = data[:, 11]
-feature5 = data[:, 12]
-features = np.concatenate((feature1,feature2, feature3, feature4, feature5), axis=-1)
+feature1 = data[:, 7:8]
+feature2 = data[:, 9:12]
+features = np.concatenate((feature1,feature2), axis=1)
 print features.shape
 
 labels = data[:, 13].ravel()
